9c5fa2e00305e1493db7609673c892f2ac4ac834
brunoqueirozrs <brunoqueirozrs@gmail.com>
2025-12-16 18:05:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
  */
 
 // CONFIGURAÇÃO
-const DEPLOY_ID = 'AKfycbxMuP7gF6WM3syD4dpraqkMPRpInQ2xkc5_09o3fuNBIHTCn8UVQFRdPpH4wiVpccvz'; 
+const DEPLOY_ID = 'AKfycbzKhx7CpfjmNEd3MqpkJBU6dGK8djfC4jmWkrxh-rWaNsruZdzhWy1fsgo3Q75H-NgN'; 
 const API_URL = `https://script.google.com/macros/s/${DEPLOY_ID}/exec`;
 const TOKEN = "MHNET2025#SEG";
 const GEMINI_KEY = "AIzaSyD8btK2gPgH9qzuPX84f6m508iggUs6Vuo"; 
