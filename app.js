372ea3eaea0bf834604c426a3cf7f47b9d956d7c
brunoqueirozrs <brunoqueirozrs@gmail.com>
2025-12-17 13:25:01 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
  */
 
 // ⚠️ ID ATUALIZADO
-const DEPLOY_ID = 'AKfycbwcA8S8xJZWYWgA6UueGOLJcB75iKAMTaVjc5B0wPfUlcalyy7hZ3YNeinIVwTNvCd5'; 
+const DEPLOY_ID = 'AKfycbyprSsQFXsywlgFfZtLSR5Flra_UZAyHUrlUG8eT5adMKNwzX_XXUyxyFtFyag5Lrgr'; 
 const API_URL = `https://script.google.com/macros/s/${DEPLOY_ID}/exec`;
 const GEMINI_KEY = "AIzaSyD8btK2gPgH9qzuPX84f6m508iggUs6Vuo"; 
 
