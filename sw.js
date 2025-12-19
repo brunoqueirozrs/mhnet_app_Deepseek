1dd4aa419d869c40d80ad5e1dbce5b899709c4b1
brunoqueirozrs <brunoqueirozrs@gmail.com>
2025-12-19 16:03:32 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,49 +1,20 @@
-const CACHE_NAME = 'mhnet-v17-cache';
-const urlsToCache = [
+const CACHE_NAME = 'mhnet-v30';
+const ASSETS = [
   './',
   './index.html',
   './app.js',
-  './manifest.json',
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
-// 1. Instalação: Guarda os arquivos no celular
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache aberto: App pronto para rodar offline');
-        return cache.addAll(urlsToCache);
-      })
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// 2. Requisições: Serve os arquivos guardados se estiver sem internet
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-// 3. Ativação: Limpa caches antigos quando o app atualiza
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
